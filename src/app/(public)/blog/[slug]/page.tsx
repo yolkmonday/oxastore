0e188e122afa2d50a8e3184709c6c85df90e0eb9
yolkmonday <aripadrian01@gmail.com>
2026-03-15 00:10:07 +0700

    fix: add force-dynamic to blog detail page

--- src/app/(public)/blog/[slug]/page.tsx
+++ src/app/(public)/blog/[slug]/page.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import { getPostBySlug } from "@/data/posts";
 import PostContentWrapper from "@/components/blog/PostContentWrapper";
 
+export const dynamic = "force-dynamic";
+
 interface Props {
   params: Promise<{ slug: string }>;
 }
